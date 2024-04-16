d2bab3dfbb8a2e81b80610ebe7d03cc84f4adb85
Ahmed-Anjum <ahmedanjum100@gmail.com>
2024-04-16 13:34:31 -0700

    push my atm to github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# 240416-atm" 
 "# 240416-atm" 
+"# Simple-Calculator" 
---
+++ index.ts
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 1500;
+let myIncome = 10000;
+let myPin =  1234;
+let pinAnswer = await inquirer.prompt(
+
+[
+    {
+    name: "pin",
+    message:"enter your pin",
+    type: "number"
+
+}
+]
+);
+if(pinAnswer.pin != myPin){
+    console.log("Incorrect Pin code");
+}
+else{
+
+
+    console.log("Correct pin code !!");
+  
+    
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message:"please select option",
+                type:"list",
+                choices: ["withdraw", "check balance", "fast cash"]
+            }
+        ]
+    )
+    console.log(operationAns)
+    if (operationAns.operation === "fast cash") {
+        let fastcashAns = await inquirer.prompt(
+            [
+                {
+                    name: "fastcash",
+                    message: "enter fast cash amount",
+                    type:"rawlist",
+                    choices: [500, 1000, 1500, 2000]
+                                }
+            ]
+       )
+      {if (fastcashAns.fastcash>myBalance){
+      console.log("insufficient balance");
+        }
+      else{
+       myBalance -= fastcashAns.fastcash ; 
+       console.log("your remaining balance after fast cash : " +myBalance)
+    }}
+}
+    
+    else{
+
+        if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type:"number"
+                                }
+            ]
+        )
+            {if (myBalance<amountAns.amount){
+            console.log("insufficient balance");
+            } 
+            else{
+            myBalance -= amountAns.amount ;
+            console.log("your remaining balance is:  "+myBalance)}}}
+            else{
+    
+     if(operationAns.operation = "check balance")
+        console.log("your balance is : " + myBalance)}}}
+    
